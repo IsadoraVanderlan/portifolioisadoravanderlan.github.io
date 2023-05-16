b86c32aeb64ae4dd15891d0a4ce00816d19b1718
Isadora Vanderlan <vanderlansantos1991@gmail.com>
2023-05-15 21:00:46 -0300

    incluido animacao

---
+++ script.js
@@ -0,0 +1,22 @@
+// ANIMAÇÃO
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.texto-intro', { 
+    rotate: {x: 0, y:20, z: 0},
+    duration:3000
+});
+
+sr.reveal('.experiencia', { 
+    rotate: {x: 0, y: 20, z: 0},
+    duration:4000
+});
+
+sr.reveal('.formacao', { 
+    rotate: {x: 0, y: 20, z: 0},
+    duration:4000
+});
+
+sr.reveal('.projeto-list', { 
+    rotate: {x: 0, y: 20, z: 0},
+    duration:4000
+});
