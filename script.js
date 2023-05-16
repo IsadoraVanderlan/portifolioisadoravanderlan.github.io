9f08853e5f8d6f125ca95f952ed5ee602ad65329
Isadora Vanderlan <vanderlansantos1991@gmail.com>
2023-05-15 21:13:34 -0300

    alterado animacao de entrada

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ sr.reveal('.texto-intro', {
     duration:3000
 });
 
-sr.reveal('.experiencia', { 
-    rotate: {x: 0, y: 20, z: 0},
+sr.reveal('.expe-texto', { 
+    rotate: {x: 0, y: 50, z: 0},
     duration:4000
 });
 
 sr.reveal('.formacao', { 
-    rotate: {x: 0, y: 20, z: 0},
-    duration:4000
+    rotate: {x: 50, y: 0, z: 0},
+    duration:3000
 });
 
 sr.reveal('.projeto-list', { 
